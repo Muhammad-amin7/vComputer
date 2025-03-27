fe555dc40eeed87a71765e7cfdae2647897259c7
Muhammad-amin7 <abduhalilovshohruh681@gmail.com>
2025-03-28 00:02:00 +0500

    Fix Netlify deployment issues

---
+++ src/Constants/UserData.jsx
@@ -0,0 +1,11 @@
+export const userData = {
+      name: null,
+      image: null,
+      telephone: null,
+      email: null,
+      deliveryMethod: {
+            city: null,
+            deliveryType: null,
+            address: null
+      },
+}
--- src/pages/Personal account/Personal.jsx
+++ src/pages/Personal account/Personal.jsx
@@ -1,9 +1,38 @@
+import { Col, Container, Row } from 'react-bootstrap'
 import './Personal.scss'
+import { FaXRay } from 'react-icons/fa'
+import userPhoto from "../../assets/img/userImg.svg"
+import { userData } from '../../Constants/UserData'
 
 export default function Personal() {
       return (
-            <section className='presonal'>
-                  <h1>HELLLLLL</h1>
+            <section className='personal'>
+                  <Container >
+                        <Row>
+                              <Col xs={3}>
+                                    <ul>
+                                          <li><FaXRay />История заказов</li>
+                                          <li><FaXRay />Избранное</li>
+                                          <li><FaXRay />Просмотренные товары</li>
+                                          <li><FaXRay />Мои отзывы</li>
+                                          <li className='active'><FaXRay />Личные данные</li>
+                                    </ul>
+                              </Col>
+
+                              <Col xs={9} >
+                                    <h1>Личные данные</h1>
+                                    <div className="user" >
+                                          <div className="image">
+                                                <img src={userData.image ? userData.image : userPhoto} alt="" />
+                                          </div>
+
+                                          <div className="info">
+
+                                          </div>
+                                    </div>
+                              </Col>
+                        </Row>
+                  </Container>
             </section>
       )
 }
